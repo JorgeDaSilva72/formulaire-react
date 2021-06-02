e937450032bb6d1422e946402baba575b87fd3e9
Jorge Da Silva <jorge.dasilva1@outlook.fr>
2021-06-02 18:35:25 +0200

    integration de emailJS dans le formulaire

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Contact.css';
+import Contact from './Contact';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Contact />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+

59e22b5282f894fc9078846461f83f12a022c27e
Jorge Da Silva <jorge.dasilva1@outlook.fr>
2021-06-03 11:04:27 +0200

    integration de la logique pour champs obligatoires et email valide

--- src/Contact.js
+++ src/Contact.js
@@ -8,26 +8,72 @@ const Contact = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  
+  const isEmail = () => {
+    let mail = document.getElementById('not-mail');
+    let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (email.match(regex)) {
+      mail.style.display = 'none';
+      return true;
+    } else {
+      mail.style.display = 'block';
+      mail.style.animation = 'dongle 1s';
+      setTimeout(() => {
+        mail.style.animation = 'none';
+      }, 1000);
+      return false;
+    }
+  }
+
+  const failMessage = (message) => {
+    let formMess = document.querySelector('.form-message');
+    formMess.innerHTML = message;
+    formMess.style.opacity = '1';
+    formMess.style.background = 'rgb(253,87,87)';
+    document.getElementById('name').classList.add('error');
+    document.getElementById('email').classList.add('error');
+    document.getElementById('message').classList.add('error');
+
+  }
+
+  const successMessage = () => {
+    let formMess = document.querySelector('.form-message');
+    formMess.innerHTML = 'Message envoyé ! Nous vous recontacterons dès que possible.';
+    formMess.style.opacity = '1';
+    formMess.style.background = '#00c1ec';
+    document.getElementById('name').classList.remove('error');
+    document.getElementById('email').classList.remove('error');
+    document.getElementById('message').classList.remove('error');
+
+    setTimeout(() => {
+      formMess.style.opacity = '0';
+    }, 5000);
+  }
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    sendFeedback("template_aqm8vac", {
-      name,
-      company,
-      phone,
-      email,
-      message,
-    });
+    if (isEmail() && name && message) {
+
+      sendFeedback("template_aqm8vac", {
+        name,
+        company,
+        phone,
+        email,
+        message,
+      });
+    } else {
+      failMessage('Merci de remplir les champs requis *');
+    }
   };
 
-  const sendFeedback = ( templateId, variables) => {
+  const sendFeedback = (templateId, variables) => {
 
     window.emailjs
       .send('service_x4kjrlp', templateId, variables,)
       .then((res) => {
-        console.log('success !');
+        successMessage();
         setName("");
         setCompany("");
         setPhone("");
@@ -35,9 +81,11 @@ const Contact = () => {
         setMessage("");
       })
       .catch(
-        (err) =>
-          document.querySelector('.form-message').innerHTML =
-            "Une erreur s'est produite, veuillez réessayer.")
+        (err) => {
+          failMessage("Une erreur s'est produite, veuillez réessayer.");
+        }
+      );
+
   };
 
   return (
